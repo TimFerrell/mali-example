e4fcb4e37e4b11c71e976e5b1c2b85e2ae3c14a2
Tim Ferrell <timothy.ferrell@auth0.com>
2018-10-10 22:55:13 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const Mali = require("Mali");
+
+const PROTO_PATH = path.resolve(__dirname, "./protos/todo.proto");
+
+const todosCollection = [];
+
+const createTodo = async ctx => {
+  // Grab the todo from the request
+  const { todo: todoPayload } = ctx.request.req;
+
+  // Add an ID
+  todoPayload.id = todosCollection.length;
+
+  // Push to the array
+  todosCollection.push(todoPayload);
+
+  // Set the response to be the mutated todo
+  ctx.res = {
+    todo: todoPayload
+  };
+};
+
+const getTodos = async ctx => {
+  ctx.res = {
+    todos: todosCollection
+  };
+};
+
+const updateTodo = async ctx => {
+  // Grab the todo from the request
+  const { todo: todoPayload } = ctx.request.req;
+
+  // Find the todo to update
+  const todoIndex = todosCollection.findIndex(
+    todo => todoPayload.id === todo.id
+  );
+
+  // Only update if found
+  if (todoIndex !== -1) {
+    todosCollection[todoIndex].done = todoPayload.done;
+    todosCollection[todoIndex].name = todoPayload.name;
+  }
+
+  ctx.res = {
+    todo: todosCollection[todoIndex]
+  };
+};
+
+const main = () => {
+  const app = new Mali(PROTO_PATH);
+  app.use({ createTodo, getTodos, updateTodo });
+  app.start("127.0.0.1:50051");
+  console.log("Listening...");
+};
+
+main();
