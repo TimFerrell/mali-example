c5d4363fe039a7e99a45769b4c0fda87012084c0
Tim Ferrell <timothy.ferrell@auth0.com>
2019-01-07 17:00:32 -0500

    Server listen local only.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PROTO_PATH = path.resolve(__dirname, "./protos/hello.proto");
 
 const echo = async ctx => {
   console.log("Received request.");
-  console.log(ctx.res);
   ctx.res = {
     message: ctx.request.req.message,
     timestamp: Date.now()
@@ -17,7 +16,7 @@ const main = () => {
     defaults: true
   });
   app.use({ echo });
-  app.start("0.0.0.0:50051");
+  app.start("127.0.0.1:50051");
   console.log("Listening...");
 };
 
